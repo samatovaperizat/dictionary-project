c89d5a9325c78eeeb0193014ca524103869c12a9
Perizat <samatovaperizat@gmail.com>
2022-03-20 15:45:23 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import "./App.css";
 import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <main>
           <Dictionary/>
         </main>
-    <footer className='App-footer'><small>Coded by Perizat Moldobaeva</small></footer>
+    <footer className="App-footer"><small>Coded by Perizat Moldobaeva</small></footer>
     </div>
     </div>
   );
