5ed6b77b986002d96709264bb6e854553edd219e
Perizat <samatovaperizat@gmail.com>
2022-03-20 15:21:55 +0100

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,14 +5,14 @@ export default function Dictionary(){
     let [keyword, setKeyword]= useState("");
 
 function handleResponse(response){
-    console.log(response);
+    console.log(response.data[0]);
 }
 
 
 function search(event){
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
-    let apiUrl= "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+    let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
 }
 
