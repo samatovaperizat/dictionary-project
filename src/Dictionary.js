8c2a7dea648ee646fbf830c00c4db2544f664302
Perizat <samatovaperizat@gmail.com>
2022-03-20 15:06:33 +0100

    added API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 export default function Dictionary(){
     let [keyword, setKeyword]= useState("");
 
-
+function handleResponse(response){
+    console.log(response);
+}
 
 
 function search(event){
     event.preventDefault();
     alert(`Searching for ${keyword} definition`);
+    let apiUrl= "https://api.dictionaryapi.dev/api/v2/entries/en/hello";
+    axios.get(apiUrl).then(handleResponse);
 }
+
+
 
 function handleKeywordChange(event){
     setKeyword(event.target.value);
