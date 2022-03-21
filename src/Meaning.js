83fc7a9f2739350ae2d24d002ca94868e02d6d76
Perizat <samatovaperizat@gmail.com>
2022-03-21 21:22:56 +0100

    added the synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Synonyms from "./Synonyms";
 export default function Meaning(props){
    console.log(props.meaning)
     return(
@@ -7,12 +7,16 @@ export default function Meaning(props){
         <h3>{props.meaning.partOfSpeech}</h3>
         {props.meaning.definitions.map(function(definition, index) {
               return (
-                
                  <div key={index}> 
-                 {definition.definition}
+                  <p>
+                <strong>Definition:</strong> {definition.definition}
                  <br/>
+                <strong>Example:</strong> 
                  <em>{definition.example} </em>
+                 <br/>
+                 <Synonyms synonyms={definition.synonyms}/>
                 
+                </p>
                  </div>);
                  
         })}
