f5dc5c691911b9b8272eedaad7466f61e42038a0
Perizat <samatovaperizat@gmail.com>
2022-03-22 21:31:43 +0100

    working  on additing photos API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,16 +8,27 @@ export default function Dictionary(props){
     let [keyword, setKeyword]= useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
-function handleResponse(response){
+function handleDictionaryResponse(response){
     setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response) {
+    setPhotos(response.data.photos)
 }
 
 function search(){
     let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
-    axios.get(apiUrl).then(handleResponse);
-}
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey ="563492ad6f9170000100000128866f61e2154e2e9837ab6be6848019";
+     let pexelsApiUrl =`https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+     let headers ={Authorization: `Bearer ${pexelsApiKey}`};
+     axios.get(pexelsApiUrl, { headers: headers })
+     .then(handlePexelsResponse);
+    }
 
 
 function handleSubmit(event){
