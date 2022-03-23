2949e4abfd4ba890d993d3824d11256077e09bb4
Perizat <samatovaperizat@gmail.com>
2022-03-23 19:51:06 +0100

    again trying to deploy

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props){
                 return (
                     <div className="col-4" key={index}>
                     <a href={photo.src.original} target="_blank" rel="noopener noreferrer">
-                    <img src={photo.src.landscape} className="img-fluid" alt={photo.alt}/>
+                    <img src={photo.src.landscape} className="img-fluid" alt=""/>
                 </a>
                 </div>
                    );
